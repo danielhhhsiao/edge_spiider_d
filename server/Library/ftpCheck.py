16230939e532e23b76f7fd07713b5299c6f3e02c
danielhhhsiao <105897793+danielhhhsiao@users.noreply.github.com>
2023-04-07 11:43:03 +0800

    Add files via upload
    add timeout 3 sec

--- server/Library/ftpCheck.py
+++ server/Library/ftpCheck.py
@@ -3,9 +3,15 @@ import io
 import sys
 
 def ftpCheckStatus(ip, proxy, user, passwd):
+	print('Hostname: '+ip)
+	print('Proxy: '+proxy)
+	print('user: '+user)
+	print('password: '+passwd)
+	print('')
+	print('=====Test Result=====')
 	fileName = "testFile"
 	try:
-		ftp = ftplib.FTP(ip, user, passwd)
+		ftp = ftplib.FTP(ip, user, passwd, timeout=3)
 		print("FTP connect success.")
 		ftp.encoding = "utf-8"
 	except:
@@ -46,5 +52,6 @@ if __name__=="__main__":
 	ip=str(sys.argv[1])
 	user=str(sys.argv[2])
 	pwd=str(sys.argv[3])
+	
 	
 	ftpCheckStatus(ip,'1',user,pwd)
